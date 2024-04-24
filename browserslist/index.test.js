e199e42599b3d9b8daf1b4c783b193a7519a3cac
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2024-04-24 20:53:30 +0200

    :heavy_minus_sign: Use native node testing

--- browserslist/index.test.js
+++ browserslist/index.test.js
@@ -1,17 +1,18 @@
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'node:test';
+import { deepStrictEqual } from 'node:assert';
 import { parseBrowserAndVersion } from './index.js';
 
 describe('#parseBrowserAndVersion', () => {
   it('should return empty values if empty line', () => {
-    expect(parseBrowserAndVersion('')).toEqual({ name: undefined, version: undefined });
+    deepStrictEqual(parseBrowserAndVersion(''), { name: undefined, version: undefined });
   });
 
   it('should parse browser and version', () => {
-    expect(parseBrowserAndVersion('firefox 121')).toEqual({ name: 'firefox', version: '121' });
-    expect(parseBrowserAndVersion('chrome 120')).toEqual({ name: 'chrome', version: '120' });
-    expect(parseBrowserAndVersion('and_chr 120')).toEqual({ name: 'and_chr', version: '120' });
-    expect(parseBrowserAndVersion('ios_saf 17.2')).toEqual({ name: 'ios_saf', version: '17.2' });
-    expect(parseBrowserAndVersion('kaios 3.0-3.1')).toEqual({ name: 'kaios', version: '3.0-3.1' });
-    expect(parseBrowserAndVersion('op_mini all')).toEqual({ name: 'op_mini', version: 'all' });
+    deepStrictEqual(parseBrowserAndVersion('firefox 121'), { name: 'firefox', version: '121' });
+    deepStrictEqual(parseBrowserAndVersion('chrome 120'), { name: 'chrome', version: '120' });
+    deepStrictEqual(parseBrowserAndVersion('and_chr 120'), { name: 'and_chr', version: '120' });
+    deepStrictEqual(parseBrowserAndVersion('ios_saf 17.2'), { name: 'ios_saf', version: '17.2' });
+    deepStrictEqual(parseBrowserAndVersion('kaios 3.0-3.1'), { name: 'kaios', version: '3.0-3.1' });
+    deepStrictEqual(parseBrowserAndVersion('op_mini all'), { name: 'op_mini', version: 'all' });
   });
 });
--- index.test.js
+++ index.test.js
@@ -1,12 +1,13 @@
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'node:test';
+import { deepStrictEqual } from 'node:assert';
 import { getBrowserslistLimitVersions } from './index.js';
 
 describe('#getBrowserslistLimitVersions', () => {
   it('should parse browsers and versions', () => {
-    expect(getBrowserslistLimitVersions('chrome 120\nfirefox 120\n')).toEqual({ chrome: '120', firefox: '120' });
-    expect(getBrowserslistLimitVersions('firefox 121\nfirefox 120\nfirefox 119')).toEqual({ firefox: '119' });
-    expect(getBrowserslistLimitVersions('ios_saf 17.2\nios_saf 17.1')).toEqual({ ios_saf: '17.1' });
-    expect(getBrowserslistLimitVersions('kaios 3.3-3.4\nkaios 3.2\nkaios 3.0-3.1')).toEqual({ kaios: '3.0-3.1' });
-    expect(getBrowserslistLimitVersions('op_mini all')).toEqual({ op_mini: 'all' });
+    deepStrictEqual(getBrowserslistLimitVersions('chrome 120\nfirefox 120\n'), { chrome: '120', firefox: '120' });
+    deepStrictEqual(getBrowserslistLimitVersions('firefox 121\nfirefox 120\nfirefox 119'), { firefox: '119' });
+    deepStrictEqual(getBrowserslistLimitVersions('ios_saf 17.2\nios_saf 17.1'), { ios_saf: '17.1' });
+    deepStrictEqual(getBrowserslistLimitVersions('kaios 3.3-3.4\nkaios 3.2\nkaios 3.0-3.1'), { kaios: '3.0-3.1' });
+    deepStrictEqual(getBrowserslistLimitVersions('op_mini all'), { op_mini: 'all' });
   });
 });
